8a32369d1e8dd4d07f10c9297675504859012ffe
tasayak <sayaka.t.0907@gmail.com>
2023-11-11 19:51:39 +0900

    Javaプログラミング学習終了

---
+++ src/UseValiable.java
@@ -0,0 +1,11 @@
+
+public class UseValiable {
+
+    public static void main(String[] args) {
+        int number2 = 100;
+        String str2 = "xyz";
+        System.out.println(number2);
+        System.out.println(str2);
+    }
+
+}
