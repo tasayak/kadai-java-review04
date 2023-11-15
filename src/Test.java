464cad3329bec0674906865548f8dcf18c3309e5
tasayak <sayaka.t.0907@gmail.com>
2023-11-15 22:44:50 +0900

    こんばんはのメッセージ追加

--- src/Test.java
+++ src/Test.java
@@ -5,6 +5,7 @@ public class Test {
         System.out.println("TechAcademy");
         System.out.println("Java");
         System.out.println("こんにちは");
+        System.out.println("こんばんは");
     }
 
 }
